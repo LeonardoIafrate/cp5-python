400518bc42fc58cfba21d23cd1f9eacc08e610c9
britojulia <juliabri1234@gmail.com>
2024-10-26 13:24:13 -0300

    adicionando funções para crud da venda

--- bd_livraria/venda.py
+++ bd_livraria/venda.py
@@ -4,15 +4,88 @@ import sqlalchemy
 from connection import *
 
 def cadastrar_venda():
+    data_venda = input("Digite a data da venda: ")
+    nome_cliente = input("Digite o nome do cliente")
+    cur.execute(
+    """
+    INSERT INTO VENDA(DATA_VENDA, NOME_CLIENTE)
+    VALUES (:DATA_VENDA, :NOME_CLIENTE)
+    """, {"DATA_VENDA": data_venda, "NOME_CLIENTE": nome_cliente}
+    )
+    con.commit()
     print()
-    #   titulo_livro = input("Digite o título do livro: ")
-    # qnt_pag = input("Digite a quantidade de páginas do livro: ")
-    # id_autor = input("Digite o ID do autor: ")
-    # qnt_estoque = input("Digite a quantidade do livro no estoque: ")
-    # preco = input("Digite o preço do livro: ")
-    # cur.execute(
-    # """
-    # INSERT INTO LIVRO( Titulo, Qnt_pag, ID_autor, Qnt_estoque, Preco)
-    # VALUES(: :titulo_livro, :qnt_pag, :id_autor, :qnt_estoque, :preco)
-    # """,{"titulo_livro": titulo_livro, "qnt_pag": qnt_pag, "id_autor":id_autor, "qnt_estoque": qnt_estoque} 
-    # )
+
+def cadastrar_venda_livro():
+    Id_venda = input("Digite o id da venda: ")
+    id_livro = input("Digite o id do livro: ")
+    quantidade = input ("Digite a quantidade de livros: ")
+    cur.execute(
+    """
+    INSERT INTO VENDA_LIVROS(ID_VENDA, ID_LIVRO, QUANTIDADE)
+    VALUES (:ID_VENDA, :ID_LIVRO, :QUANTIDADE)
+    """, {"ID_VENDA": Id_venda, "ID_LIVRO": id_livro, "QUANTIDADE": quantidade}
+    )
+    con.commit()
+    print()
+
+def altera_venda_livro(id_venda: int, id_livro: int, quantidade: int):
+    cur.execute("SELECT * FROM VENDA_LIVROS WHERE ID_VENDA = :id_venda", {"id_venda": id_venda})
+    venda = cur.fetchone()
+    
+    if venda is None:
+        raise HTTPException(status_code=404, detail="venda livro não encontrado")
+    
+    try:
+        cur.execute(
+        """
+        UPDATE VENDA_LIVROS SET id_venda = :id_venda, id_livro = :id_livro, quantidade = :quantidade WHERE ID_VENDA = :id_venda
+        """, {"id_venda": id_venda, "id_livro": id_livro, "quantidade": quantidade}
+        )
+        con.commit()
+        return {"Message": "Venda livro alterada com sucesso"}
+    except Exception as e:
+        return {"Erro": f"Um erro inesperado aconteceu, {str(e)}"}
+    
+    
+def altera_venda(id_venda: int, data_venda: int, nome_cliente: str):
+    cur.execute("SELECT * FROM VENDA WHERE ID_VENDA = :id_venda", {"id_venda": id_venda})
+    venda = cur.fetchone()
+    
+    if venda is None:
+        raise HTTPException(status_code=404, detail="venda não encontrado")
+    
+    try:
+        cur.execute(
+        """
+        UPDATE VENDA SET data_venda = :data_venda, nome_cliente = :nome_cliente WHERE ID_VENDA = :id_venda
+        """, {"id_venda": id_venda, "nome_cliente": nome_cliente, "data_venda": data_venda}
+        )
+        con.commit()
+        return {"Message": "Venda alterada com sucesso"}
+    except Exception as e:
+        return {"Erro": f"Um erro inesperado aconteceu, {str(e)}"}
+
+def excluir_venda():
+    try:
+        Id_venda = input("Digite o id da venda que deseja excluir: ")
+        confirmacao = input(f"Tem certeza que deseja excluir a venda {Id_venda}? (S/N)").upper
+        cur.execute("SELECT ID_VENDA FROM VENDA WHERE ID_VENDA = :ID_VENDA", {"ID_VENDA": Id_venda})
+        listar_venda = cur.fetchone()
+        if listar_venda:
+            if confirmacao == "S":
+                cur.execute(
+                """
+                DELETE FROM VENDA WHERE ID_VENDA = :id_venda
+                """, {"id_venda": Id_venda})
+                cur.execute(
+                """
+                DELETE FROM VENDA_LIVRO WHERE ID_VENDA = :id_venda
+                """, {"id_venda": Id_venda})
+                con.commit()
+            elif confirmacao == "N":
+                print("Operação cancelada!")
+        raise KeyError("venda não cadastrada")
+    except KeyError as e:
+        print("Erro ao excluir venda: ", e)
+
+
